7f65d5ac8b893121eb092f1977c09867b14ee99e
Ryan Kelleher <ryan.kelleher1@mycit.ie>
2020-11-20 14:22:33 +0000

    Connected models to redesigned website

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const admin = require("firebase-admin");
 var model = require('./loan-model.js');// require model from loan-model.js
-var houseModel = require('./house-model.js');// require model from loan-model.js
+var houseModel = require('./house-model.js');// require model from house-model.js
 const tf = require('@tensorflow/tfjs-node');
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 app.use('/static', express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}));
-//app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.get('/calculator', function (req, res) {
@@ -21,7 +22,7 @@ app.get('/calculator', function (req, res) {
 
 //post method that takes data from form and uses loan model to make a prediction - edited by Ryan 
 app.post('/calculator',function(req,res){
-	console.log("POST");
+	console.log(req.body.gender);
    var gender = parseFloat(req.body.gender);
    var married = parseFloat(req.body.married);
    var graduated = parseFloat(req.body.graduated);
@@ -31,14 +32,8 @@ app.post('/calculator',function(req,res){
    var amount = parseFloat(req.body.amount);
    var time = parseFloat(req.body.time);
    var creditHistory = parseFloat(req.body.creditHistory);
-   var area = req.body.area;
-   
-   if(area == "Urban")
-		area = 3;
-	else if(req.body.area == "Semiurban")
-		area = 2;
-	else
-		area = 1;
+   var area = parseFloat(req.body.area);
+
 	
    var dependents = parseFloat(req.body.dependents);
    
@@ -48,15 +43,19 @@ app.post('/calculator',function(req,res){
    
    console.log(sample);
    console.log(prediction);
-   //console.log(result);
+   
    var p = Promise.resolve(prediction);
    
-   p.then(function(result) {
-	   console.log(result);
+  /*  p.then(function(result) {
 	if(result == 1)
-		res.send("You Qualify For This Loan");
+		res.render("qualify");
 	else
 		res.send("You Do Not Qualify For This Loan");
+	}); */
+	
+	p.then(function(result) {
+		var profile = sample;
+		res.render("qualify", {result: result, profile: sample});
 	});
 
 }); //end post method 
@@ -68,6 +67,7 @@ app.get("/home", function (req, res) {
 app.get("/value", function (req, res) {
     res.render("value.html");
 });
+
 //post method that takes data from form and uses house model to make a prediction - edited by Ryan 
 app.post('/value',function(req,res){
 	console.log("POST");
@@ -80,9 +80,9 @@ app.post('/value',function(req,res){
    var view = parseFloat(req.body.view);
    var condition = parseFloat(req.body.condition);
    var yrBuilt = parseFloat(req.body.yrBuilt);
-   var renovated = parseFloat(req.body.renovated);
-   var aboveSqFt = parseFloat(req.body.aboveSqFt);
+   var renovated = parseFloat(req.body.renovated);   
    var belowSqFt = parseFloat(req.body.belowSqFt);
+   var aboveSqFt = sqfliving - belowSqFt;
    var waterfront = parseFloat(req.body.waterfront);	
    
    var sample = [bedrooms, bathrooms, sqfliving, sqflot, floors, waterfront, view, condition, aboveSqFt, belowSqFt, yrBuilt, renovated]
@@ -91,7 +91,8 @@ app.post('/value',function(req,res){
 	h.then(model => {
 		let prediction= model.predict(tf.tensor(sample, [1, sample.length])).arraySync();
 		console.log(prediction);
-		res.send(prediction[0])	
+		var houseDetails = sample;
+		res.render("housePrice", {value: prediction[0], houseDetails: houseDetails});
 	});  
 
 }); //end post method 
